c4b08c7b7831bb211bfa3f5ef6069769978a1451
23z279-yogesh <23z279@psgtech.ac.in>
2026-08-12 07:54:22 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+        int count=1;
+        boolean res = false;
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                   
+                   count++;
+                }
+                if(count>1){
+                    res= true;
+                }
+                else{
+                    res= false;
+                }
+            }
+        }
+    return res;
+        
+    }
+}
